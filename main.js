20939d3098b124686fc8fdf0ce1ed06df631f4f7
nasrullah <nasrullah.saloji@gmail.com>
2022-06-08 16:35:21 +0800

    add addEventListener, function game(), getComputerChoice()

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // cache the DOM
-const playerScores = 0;
-const computerScores = 0;
+let playerScore = 0;
+let computerScore = 0;
+
 const resultInfo_h2 = document.getElementById('result-info');
 const resultMessage_h3 = document.getElementById('result-message');
 const playerSign_div = document.getElementById('player-sign');
@@ -11,3 +12,30 @@ const rockBtn = document.getElementById('rock-btn');
 const paperBtn = document.getElementById('paper-btn');
 const scissorsBtn = document.getElementById('scissors-btn');
 const endgameMsg_p = document.getElementById('endgame-msg');
+
+//add function getComputerChoice
+function getComputerChoice() {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomChoice = Math.floor(Math.random() * 3);
+    return choices[randomChoice]
+}
+
+//add function game(). THis function will run the game logic
+function game(playerChoice) {
+    const computerChoice = getComputerChoice();
+    console.log("player==>" + playerChoice)
+    console.log("computer==>" + computerChoice);
+}
+//addEventListener. This will run the function game() after click
+function main() {
+    rockBtn.addEventListener('click', function () {
+        game("rock")
+    })
+    paperBtn.addEventListener('click', function () {
+        game("paper")
+    })
+    scissorsBtn.addEventListener('click', function () {
+        game("scissors")
+    })
+}
+main();
